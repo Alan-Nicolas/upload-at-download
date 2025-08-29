9c1a29a393152ba5818a661449063c3e8e5f7014
Alan Nicolas <alanicolax@gmail.com>
2025-08-28 23:59:25 -0400

    teste commit

---
+++ src/main/java/controller/FileController.java
@@ -0,0 +1,1 @@
+package controller;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("/file")public class FileController {}
